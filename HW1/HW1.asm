68c0ca06a034f39a170e9c966f50a905d4e82c7d
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2016-09-15 11:50:42 -0400

    Began HW1

--- HW1/HW1.asm
+++ HW1/HW1.asm
@@ -1,1 +1,27 @@
-#Blank hw1 slate
+# Homework #1
+# name: Kuba Gasiorowski
+# sbuid: 109776237
+
+.data
+.align 2
+numargs: .word 0
+arg1: .word 0
+arg2: .word 0
+arg3: .word 0
+Err_string: .asciiz "ARGUMENT ERROR"
+
+# Helper macro for grabbing command line arguments
+.macro load_args
+	sw $a0, numargs
+	lw $t0, 0($a1)
+	sw $t0, arg1
+	lw $t0, 4($a1)
+	sw $t0, arg2
+	lw $t0, 8($a1)
+	sw $t0, arg3
+.end_macro
+
+.text
+.globl main
+main:
+	load_args() # Only do this once
