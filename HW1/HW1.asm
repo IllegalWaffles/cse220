48c12ce5da34d57d74213ad15b86dcaeffbd3eb2
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2016-09-18 02:00:48 -0400

    Added macros

--- HW1/HW1.asm
+++ HW1/HW1.asm
@@ -9,11 +9,12 @@ arg1: .word 0
 arg2: .word 0
 arg3: .word 0
 Err_string: .asciiz "ARGUMENT ERROR"
-Goodbye_string: .asciiz "Goodbye :)"
+Goodbye_string: .asciiz "Goodbye..."
 Numargs_string: .asciiz "Number of arguments: "
 Part2_string: .asciiz "Part 2:"
 Part3_string: .asciiz "Part 3:"
-Newline: .asciiz "\n"
+newline: .asciiz "\n"
+space: .asciiz " "
 
 # Helper macro for grabbing command line arguments
 .macro load_args
@@ -26,18 +27,35 @@ Newline: .asciiz "\n"
 	sw $t0, arg3
 .end_macro
 
-.macro exit_clean
-	la $a0, Goodbye_string
+# Helps me push stuff onto the stack
+.macro push (%reg)
+	sw %reg($sp)
+	addi $sp, $sp, -4
+.end_macro
+
+# Helps me pop stuff from the stack
+.macro pop (%reg)
+	addiu $sp, $sp, 4
+	lw %reg($sp)
+.end_macro
+
+# Prints a string to console
+.macro print_string (%string)
+	la $a0, %string
 	li $v0, 4
-	syscall
-	li $v0, 10
 	syscall
 .end_macro
 
-.macro exit_argument_error
-	la $a0, Err_string
-	li $v0, 4
+# Prints an int to the console
+.macro print_int (%reg)
+	move $a0, %reg
+	li $v0, 1
 	syscall
+.end_macro
+
+# Exits the program with a specified string
+.macro exit_with_string (%string)
+	print_string(%string)
 	li $v0, 10
 	syscall
 .end_macro
@@ -47,36 +65,37 @@ Newline: .asciiz "\n"
 main:
 
 	load_args() # Only do this once
-	
-	move $t0, $a0 # Hold on to the number of arguments
+	move $s0, $a0 # Hold on to the number of arguments
 
 Print_number_args:	
-	li $v0, 4
-	la $a0, Numargs_string
-	syscall
 	
-	li $v0, 1
-	move $a0, $t0
-	syscall
-	
-	li $v0, 4
-	la $a0, Newline
-	syscall
+	print_string(Numargs_string)
+	print_int($s0)
+	print_string(newline)
 
-	bgt $t0, 3, Illegal_arguments 
-	blt $t0, 2, Illegal_arguments	
+	bgt $s0, 3, Illegal_arguments 
+	blt $s0, 2, Illegal_arguments	
 
-	lw $t1, arg1
-	lw $t2, arg2
-	lw $t3, arg3
+	la $a0, arg1
+	jal Find_length
+
+	print_string(arg1)
 
 	j Exit
 
 Illegal_arguments:
-	exit_argument_error()
+	exit_with_string(Err_string)
 
 Exit:	
-	exit_clean()
+	exit_with_string(Goodbye_string)
 	
-
+Find_length: # Expects the address of the word to be loaded into $a0
+	li $v0, 0
+loop:	
+	lb $t0, ($a0)
+	beqz $t0, exit
+	
+	
+	exit:
+	jr $ra
 	
