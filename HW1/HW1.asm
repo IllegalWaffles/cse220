b48a5773ada00d39e6bc225c3d90ef795b14f25a
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2016-09-19 11:19:12 -0400

    Completed Part 1 (started part 2)

--- HW1/HW1.asm
+++ HW1/HW1.asm
@@ -9,9 +9,9 @@ arg1: .word 0
 arg2: .word 0
 arg3: .word 0
 Err_string: .asciiz "ARGUMENT ERROR"
-Goodbye_string: .asciiz "Goodbye..."
-Part2_string: .asciiz "Part 2:"
-Part3_string: .asciiz "Part 3:"
+Goodbye_string: .asciiz "\n(clean exit)"
+Part2_string: .asciiz "Part 2: "
+Part3_string: .asciiz "Part 3: "
 endl: .asciiz "\n"
 space: .asciiz " "
 
@@ -59,6 +59,13 @@ space: .asciiz " "
 	syscall
 .end_macro
 
+# Prints an int as a char
+.macro pchar (%reg)
+	move $a0, %reg
+	li $v0, 11
+	syscall
+.end_macro
+
 # Exits the program with a specified string
 .macro exit_with_string (%string)
 	pstring(%string)
@@ -73,23 +80,52 @@ main:
 	load_args() 	# Only do this once
 	move $s0, $a0 	# Hold on to the number of arguments
 
-	bgt $s0, 3, Illegal_arguments_exit 
-	blt $s0, 2, Illegal_arguments_exit
+	bgt $s0, 3, Exit_illegal_arguments 
+	blt $s0, 2, Exit_illegal_arguments
 	
 	lw $a0, arg1
 	jal Strlen	# Determine length of arg1
 	
 	move $s1, $v0	# $s1 now holds length of arg1
 	
-	bne $s1, 1, Illegal_arguments_exit 
+	bne $s1, 1, Exit_illegal_arguments
 	
-	j Exit
+	# A = 65
+	# a = 97
+	# R = 82
+	# r = 114
+	
+	lw $t0, arg1
+	lb $s2, ($t0)	# $s2 holds arg1
+	
+	beq $s2, 65, Part2
+	beq $s2, 97, Part2
+	beq $s2, 82, Part3
+	beq $s2, 114, Part3 
+	
+	j Exit_illegal_arguments
+	
+Part2:
+	#pstring(Part2_string)
+	#pchar($s2)
+	blt $s0, 3, Exit_illegal_arguments
+	
+		
+
+	
+	
+	j Exit_clean
+
+Part3:
+	pstring(Part3_string)
+	pchar($s2)
+	j Exit_clean
 
 # Exit labels
-Illegal_arguments_exit:
+Exit_illegal_arguments:
 	exit_with_string(Err_string)
 
-Exit:	
+Exit_clean:	
 	exit_with_string(Goodbye_string)
 	
 	
