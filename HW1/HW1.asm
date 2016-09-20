3926380fe79c3d04fe5051b1313a7b878e87d9f1
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2016-09-20 15:49:55 -0400

    Annotated Part 3

--- HW1/HW1.asm
+++ HW1/HW1.asm
@@ -16,6 +16,14 @@ Part3_string: .asciiz "Part 3: "
 endl: .asciiz "\n"
 space: .asciiz " "
 hamming_distance: .asciiz "Hamming Distance: "
+last_value: .asciiz "Last value drawn: "
+total_values: .asciiz "Total values: "
+evens: .asciiz "# of Even: "
+odds: .asciiz "# of Odds: "
+powers_of_2: .asciiz "Power of 2: "
+mult2: .asciiz "Multiple of 2: "
+mult4: .asciiz "Multiple of 4: "
+mult8: .asciiz "Multiple of 8: "
 
 # Helper macro for grabbing command line arguments
 .macro load_args
@@ -167,9 +175,23 @@ Part3:
 	lw $a0, arg2
 	jal Hash
 	
-	move $a0, $v0
-	li $v0, 1
+	li $a0, 0
+	move $a1, $v0
+	li $v0, 40
 	syscall
+	
+loop:	# Generate random value
+	# Check if it's a power of 2
+	# Check if it's < 64
+	# Is both are true, branch to exit
+	# Otherwise, check if its even
+	# Check if it's odd
+	# Check if it's div by 2
+	# Check if it's div by 4
+	# Check if it's div by 8
+	# (Incrememnt a separate counter for each of these conditions)
+	# Back to loop
+exit:	# Print the collected data 
 	
 	j Exit_clean
 
@@ -188,12 +210,12 @@ Strlen:
 		 		# $a0 holds the address of our word
 	li $v0, 0 		# $v0 is our counter variable
 	
-loop:	lb $t0, ($a0)		# Loads the next byte into $t2
-	beqz $t0, exit 		# if it's a null terminator, jump to the exit
+loop_l:	lb $t0, ($a0)		# Loads the next byte into $t2
+	beqz $t0, exit_l 		# if it's a null terminator, jump to the exit
 	addi $a0, $a0, 1 	# increment our string pointer
 	addi $v0, $v0, 1 	# increment our counter
-	j loop			# LOOP!
-exit:
+	j loop_l			# LOOP!
+exit_l:
 	jr $ra
 #############################################################################
 BuildWord:
@@ -247,22 +269,16 @@ HammingDistance:
 	move $t7, $a1
 	
 	xor $t0, $t6, $t7	# XOR'd the numbers
+	move $a0, $t0		# Put it in the function argument slot
 	
-	li $t1, 0		# General counter
-	li $t2, 0		# Counter for xor bits
+	push($ra)		# Save our return address
 	
-loop_h:	bgt $t1, 31, exit_h 	# Loop counts from 0-31, to get each bit
-
-	srlv $t3, $t0, $t1 	# Shift some bits over
-	andi $t4, $t3, 1	# Mask for only the first bit
-	bne $t4, 1, zero
-	addi $t2, $t2, 1
-zero: 
-	addi $t1, $t1, 1	# Increment counter
-	j loop_h 		# LOOP!
-
-exit_h:	move $v0, $t2
-	jr $ra
+	jal TrueBits
+	
+	pop($ra)		# Recall our return address
+				# The return value of TrueBits is what we want.
+	jr $ra			# Simply return now.
+	
 #############################################################################
 Hash:
 				# $a0 holds the word we wanna hash
@@ -273,12 +289,12 @@ Hash:
 	li $t8, 57		# $t8 holds the value for '9'
 	li $t9, 10		# $t9 holds 10
 	
-loop1:	
+loop_h:	
 	lb $t1, ($t0)		# Get the next char
-	beqz $t1, exit2		# If it's a null terminator, that's the end of the string
+	beqz $t1, exit_h	# If it's a null terminator, that's the end of the string
 	
-	blt $t1, $t7, exit2	
-	bgt $t1, $t8, exit2	# Break
+	blt $t1, $t7, exit_h
+	bgt $t1, $t8, exit_h	# Break
 	
 	sub $t3, $t1, $t7 	# char - '0'
 	
@@ -288,16 +304,28 @@ loop1:
 	add $t2, $t3, $t4	# (sum * 10) + (char - '0')
 	
 	addi $t0, $t0, 1	# Increment pointer
-	j loop1			# LOOP!
-exit2:
+	j loop_h			# LOOP!
+exit_h:
 	move $v0, $t2
 	jr $ra
 #############################################################################
 TrueBits:			# Returns the number of bits that are true in the byte loaded into #a0
+				# If this returns exactly 1, then the number in $a0 is a power of 2
+				
+	li $t1, 0		# General counter
+	li $t2, 0		# Counter for xor bits
+	move $t0, $a0
 	
+loop_tb:
+	bgt $t1, 31, exit_tb 	# Loop counts from 0-31, to get each bit
 
-
-
-
-
+	srlv $t3, $t0, $t1 	# Shift some bits over
+	andi $t4, $t3, 1	# Mask for only the first bit
+	bne $t4, 1, zero
+	addi $t2, $t2, 1
+zero: 
+	addi $t1, $t1, 1	# Increment counter
+	j loop_tb 		# LOOP!
+exit_tb:
+	move $v0, $t2
 	jr $ra
