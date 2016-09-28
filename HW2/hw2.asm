64febece5daa8d229d4cc9cb5c8f9689434e036c
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2016-09-28 11:30:12 -0400

    Initial Commit for HW2

---
+++ HW2/hw2.asm
@@ -0,0 +1,78 @@
+##############################################################
+# Homework #2
+# name: MY_NAME
+# sbuid: MY_SBU_ID
+##############################################################
+.text
+
+##############################
+# PART 1 FUNCTIONS 
+##############################
+
+atoui:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, 0
+	############################################
+	jr $ra
+
+uitoa:
+    #Define your code here
+    li $v0, 0
+    li $v1, 0
+    
+    jr $ra
+
+##############################
+# PART 2 FUNCTIONS 
+##############################    
+            
+decodeRun:
+    #Define your code here
+    li $v0, 0
+    li $v1, 0
+    
+    jr $ra
+
+decodedLength:
+    #Define your code here
+    li $v0, 0
+    li $v1, 0
+    
+    jr $ra
+         
+runLengthDecode:
+    #Define your code here
+    li $v0, 0
+    
+    jr $ra
+
+
+##############################
+# PART 3 FUNCTIONS 
+##############################
+                
+encodeRun:
+    #Define your code here
+    li $v0, 0
+    li $v1, 0
+    
+    jr $ra
+
+encodedLength:
+    #Define your code here
+    li $v0, 0
+    
+    jr $ra        
+
+runLengthEncode:
+    #Define your code here
+    li $v0, 0
+    
+    jr $ra
+    
+
+
+.data 
+.align 2
