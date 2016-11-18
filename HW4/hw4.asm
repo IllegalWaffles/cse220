361423e0acaa63b9338ab6ce1535249325464c82
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2016-11-17 20:07:45 -0500

    Initial Commit for HW4

---
+++ HW4/hw4.asm
@@ -0,0 +1,100 @@
+##############################################################
+# Homework #4
+# name: MY_NAME
+# sbuid: MY_SBU_ID
+##############################################################
+.text
+
+##############################
+# PART 1 FUNCTIONS
+##############################
+
+preorder:
+    #Define your code here
+	jr $ra
+
+##############################
+# PART 2 FUNCTIONS
+##############################
+
+linear_search:
+    #Define your code here
+    ############################################
+    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+    li $v0, -10
+    ###########################################
+    jr $ra
+
+set_flag:
+    #Define your code here
+    ############################################
+    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+    li $v0, -20
+    ###########################################
+    jr $ra
+
+find_position:
+    #Define your code here
+    ############################################
+    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+    li $v0, -30
+    li $v1, -40
+    ###########################################
+    jr $ra
+
+add_node:
+    #Define your code here
+    ############################################
+    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+    li $v0, -50
+    ###########################################
+	jr $ra
+
+##############################
+# PART 3 FUNCTIONS
+##############################
+
+get_parent:
+    #Define your code here
+    ############################################
+    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+    li $v0, -60
+    li $v1, -70
+    ###########################################
+    jr $ra
+
+find_min:
+    #Define your code here
+    ############################################
+    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+    li $v0, -80
+    li $v1, -90
+    ###########################################
+    jr $ra
+
+delete_node:
+    #Define your code here
+    ############################################
+    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+    li $v0, -100
+    ###########################################
+    jr $ra
+
+##############################
+# EXTRA CREDIT FUNCTION
+##############################
+
+add_random_nodes:
+    #Define your code here
+    jr $ra
+
+
+
+#################################################################
+# Student defined data section
+#################################################################
+.data
+.align 2  # Align next items to word boundary
+
+#place any additional data declarations here
+
